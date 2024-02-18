5e89ac1e069a8d868169fba8e97a72585f3d69cc
codenamehj <loveim1212@naver.com>
2024-02-18 14:55:19 +0900

    0218 HW Complete!

--- plsql_240216.sql
+++ plsql_240216.sql
@@ -471,7 +471,7 @@ EXECUTE yedam_ju(1511013689977)
 DROP PROCEDURE yedam_ju;
 -- 프로시저, IN 매개변수 하나
 CREATE PROCEDURE yedam_ju
- (p_ju_no IN VARCHAR2)
+ (p_ju_no IN VARCHAR2,)
 IS
 -- 선언부 : 내부에서 사용할 변수, 타입, 커서 등
         v_result VARCHAR2(100);
@@ -491,7 +491,26 @@ EXECUTE yedam_ju(1511013689977);
 단, 해당사원이 없는 경우 "해당사원이 없습니다." 출력
 예) EXECUTE TEST_PRO(176)
 */
+DROP PROCEDURE TEST_PRO;
+CREATE PROCEDURE TEST_PRO
+    (p_empno IN test_employees.employee_id%TYPE) --변수 이름, 타입 설정
+IS
+-- 선언부 : 내부에서 사용할 변수, 타입, 커서 등
+    e_emp_del_fail EXCEPTION;
+BEGIN
+    DELETE FROM test_employees
+    WHERE employee_id = p_empno;
+    
+     IF SQL%ROWCOUNT = 0 THEN
+        RAISE e_emp_del_fail;
+    END IF;
+EXCEPTION
+    WHEN e_emp_del_fail THEN
+        DBMS_OUTPUT.PUT_LINE('해당 사원이 존재하지 않습니다.');
+END;
+/
 
+EXECUTE TEST_PRO(176);
 
 /* 3. 다음과 같이 PL/SQL 블록을 실행할 경우 
 사원번호를 입력할 경우 사원의 이름(last_name)의 첫번째 글자를 제외하고는
@@ -500,7 +519,25 @@ EXECUTE yedam_ju(1511013689977);
 실행) EXECUTE yedam_emp(176)
 실행결과) TAYLOR -> T*****  <- 이름 크기만큼 별표(*) 출력
 */
+DROP PROCEDURE yedam_emp;
+CREATE PROCEDURE yedam_emp
+    (p_empno IN test_employees.employee_id%TYPE) --변수 이름, 타입 설정
+IS
+-- 선언부 : 내부에서 사용할 변수, 타입, 커서 등
+    v_result VARCHAR2(100);
+    v_ename test_employees.last_name%TYPE;
+BEGIN
+    SELECT last_name
+    INTO v_ename
+    FROM test_employees
+    WHERE employee_id = p_empno;
 
+    v_result := RPAD(SUBSTR(v_ename, 1, 1), LENGTH(v_ename), '*');
+    DBMS_OUTPUT.PUT_LINE(v_result);
+END;
+/
+
+EXECUTE yedam_emp(104);
 
 /* 4. 부서번호를 입력할 경우 
 해당부서에 근무하는 사원의 사원번호, 사원이름(last_name)을 출력하는 get_emp 프로시저를 생성하시오. 
@@ -508,11 +545,67 @@ EXECUTE yedam_ju(1511013689977);
 단, 사원이 없을 경우 "해당 부서에는 사원이 없습니다."라고 출력(exception 사용)
 실행) EXECUTE get_emp(30)
 */
+DROP PROCEDURE get_emp;
+CREATE PROCEDURE get_emp
+    (p_deptid IN test_employees.department_id%TYPE) --변수 이름, 타입 설정
+IS
+-- 선언부 : 내부에서 사용할 변수, 타입, 커서 등
+    CURSOR emp_cursor(p_deptid test_employees.department_id%TYPE) IS
+        SELECT employee_id, last_name
+        FROM test_employees
+        WHERE department_id = p_deptid;
 
+    v_emp_info emp_cursor%ROWTYPE;
+    
+    e_emp_sel_fail EXCEPTION;
+BEGIN
+    OPEN emp_cursor(p_deptid); 
+        LOOP
+            FETCH emp_cursor INTO v_emp_info;
+            EXIT WHEN emp_cursor%NOTFOUND;
+            
+            DBMS_OUTPUT.PUT('사원번호 : ' || v_emp_info.employee_id);
+            DBMS_OUTPUT.PUT_LINE(', 사원이름 : ' || v_emp_info.last_name);
+        END LOOP;
+        
+        IF emp_cursor%ROWCOUNT = 0 THEN
+            RAISE e_emp_sel_fail;        
+        END IF;
+        
+        CLOSE emp_cursor;
+        
+EXCEPTION
+    WHEN e_emp_sel_fail THEN 
+        DBMS_OUTPUT.PUT_LINE('해당 부서에는 사원이 없습니다.');
+END;
+/
+
+EXECUTE get_emp(30);
 
 /* 5. 직원들의 사번, 급여 증가치만 입력하면 Employees테이블에 쉽게 사원의 급여를 갱신할 수 있는 y_update 프로시저를 작성하세요. 
 만약 입력한 사원이 없는 경우에는 ‘No search employee!!’라는 메시지를 출력하세요.(예외처리)
 실행) EXECUTE y_update(200, 10)
 */
+DROP PROCEDURE y_update;
+CREATE PROCEDURE y_update
+    (p_empno IN test_employees.employee_id%TYPE,
+     p_incrsal IN test_employees.salary%TYPE) --변수 이름, 타입 설정
+IS
+-- 선언부 : 내부에서 사용할 변수, 타입, 커서 등
+    e_emp_sel_fail EXCEPTION;
+BEGIN
+    UPDATE test_employees
+    SET salary = salary + p_incrsal
+    WHERE employee_id = p_empno;
+    
+    IF SQL%ROWCOUNT = 0 THEN
+        RAISE e_emp_sel_fail;
+    END IF;
+    
+EXCEPTION
+    WHEN e_emp_sel_fail THEN
+        DBMS_OUTPUT.PUT_LINE('No search employee!!');
+END;
+/
 
-
+EXECUTE y_update(200, 0);
